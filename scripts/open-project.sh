6b79a4bc1d1bcfecc76d11e93ca5be5f119cf3c4
Jatinder Braich <jatinder.braich@dubber.net>
2024-09-05 10:28:33 +1000

    make the open-project script work outside of the tmux session as well

--- scripts/open-project.sh
+++ scripts/open-project.sh
@@ -1,7 +1,15 @@
-projects="plan-service license-service configuration-service oboroten"
-selected_project=$(echo $projects | tr ' ' '\n' | fzf)
+#!/bin/zsh
+selected_project=$(ls ~/work | fzf)
 
 if [ ! -z "$selected_project" ]; then
     wname=$(echo $selected_project | cut -d'-' -f1)
-    tmux new-window -c ~/work/$selected_project -n $wname "nvim ."
+    if [ -n "$TMUX" ]; then
+        tmux new-window -c ~/work/$selected_project -n $wname "nvim ."
+    else
+        if ! tmux has-session -t $wname 2>/dev/null; then
+            tmux new-session -s $wname -c ~/work/$selected_project -n $wname "nvim ."
+        else
+            tmux attach-session -t $wname
+        fi
+    fi
 fi
