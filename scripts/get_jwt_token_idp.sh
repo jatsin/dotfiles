cc019dfa1f5f9b48a6376047e71f12bb51f9630b
Jatinder Singh <er.jatinder@ymail.com>
2024-12-10 14:18:31 +1100

    Add idp script (#26)
    * add idp script
    
    * sanitize

---
+++ scripts/get_jwt_token_idp.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Keycloak configuration
+# 
+case $1 in
+    "dev")
+        URL="dev.idp.dubber.net"
+        ;;
+    "qa")
+        URL="qa.idp.dubber.net"
+        ;;
+    "staging")
+        URL="staging.idp.dubber.net"
+        ;;
+    "au")
+        URL="idp.apac.dubber.net"
+        ;;
+    *)
+        echo "Usage: $0 [dev|qa|staging|prod]"
+        exit 1
+        ;;
+esac
+
+URL=$1
+KEYCLOAK_URL="https://$URL/"
+REALM="dubber"
+CLIENT_ID="dubber-spa" #au_portal
+CLIENT_SECRET=""
+USERNAME=""
+PASSWORD=""
+
+# Construct token endpoint URL
+TOKEN_URL="${KEYCLOAK_URL}/auth/realms/$REALM/protocol/openid-connect/token"
+
+# Make the token request
+echo "Requesting token..."
+response=$(curl -s -X POST "${TOKEN_URL}" \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -d "client_id=${CLIENT_ID}" \
+    -d "client_secret=${CLIENT_SECRET}" \
+    -d "grant_type=password" \
+    -d "username=${USERNAME}" \
+    -d "password=${PASSWORD}")
+
+# Check if the request was successful
+if echo "$response" | grep -q "access_token"; then
+    echo "Authentication successful!"
+    echo
+    echo "Access Token:"
+    echo "$response" | jq -r .access_token
+    
+    echo -e "\nRefresh Token:"
+    echo "$response" | jq -r .refresh_token
+    
+    echo -e "\nExpires in:"
+    echo "$response" | jq -r .expires_in
+else
+    echo "Error obtaining token:"
+    echo "$response" | jq '.' || echo "$response"
+fi
