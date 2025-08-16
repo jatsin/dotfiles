e1b51d04545a26dbdadbdbe584f5b732ae113d9f
Jatinder Braich <er.jatinder@ymail.com>
2025-08-16 22:25:53 +1000

    add some plugins and enable default lsps for webdev
    vim-tmux-navigator
    harpoon

---
+++ config/.config/nvim/lua/plugins/utils.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup()
+
+      vim.keymap.set("n", "<C-a>", function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set("n", "<C-e>", function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      for i = 1, 9 do
+        vim.keymap.set("n", "<leader>" .. i, function()
+          harpoon:list():select(i)
+        end, { desc = "Harpoon: Go to file " .. i })
+      end
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<C-p>", function()
+        harpoon:list():prev()
+      end)
+      vim.keymap.set("n", "<C-n>", function()
+        harpoon:list():next()
+      end)
+    end,
+  },
+}
