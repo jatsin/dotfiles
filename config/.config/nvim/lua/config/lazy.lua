ecf33758cafc5b7cb672ead8f68ba86c408329f2
Jatinder Braich <er.jatinder@ymail.com>
2025-08-21 10:27:02 +1000

    use default Harpoon2

--- config/.config/nvim/lua/config/lazy.lua
+++ config/.config/nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.editor.harpoon2" },
     -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
     { import = "plugins" },
--- config/.config/nvim/lua/plugins/utils.lua
+++ config/.config/nvim/lua/plugins/utils.lua
@@ -17,34 +17,4 @@ return {
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local harpoon = require("harpoon")
-      harpoon:setup()
-
-      vim.keymap.set("n", "<C-a>", function()
-        harpoon:list():add()
-      end)
-      vim.keymap.set("n", "<C-e>", function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
-
-      for i = 1, 9 do
-        vim.keymap.set("n", "<leader>" .. i, function()
-          harpoon:list():select(i)
-        end, { desc = "Harpoon: Go to file " .. i })
-      end
-
-      -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<C-p>", function()
-        harpoon:list():prev()
-      end)
-      vim.keymap.set("n", "<C-n>", function()
-        harpoon:list():next()
-      end)
-    end,
-  },
 }
